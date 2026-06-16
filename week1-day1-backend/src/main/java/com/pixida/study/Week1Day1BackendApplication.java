fea870e00afbdf6100c26195a7b7ec2da5b860d4
Klaillton <25017530+Klaillton@users.noreply.github.com>
2026-06-16 14:31:40 +0100

    initial spring boot setup with swagger

---
+++ week1-day1-backend/src/main/java/com/pixida/study/Week1Day1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.pixida.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1Day1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1Day1BackendApplication.class, args);
+	}
+
+}
